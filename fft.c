f57b2f104874500e7e3d19692bcda6b397787c8a
Enable <enableshowrun@gmail.com>
2023-07-09 20:15:58 -0700

    Lookup table added

--- fft.c
+++ fft.c
@@ -8,12 +8,18 @@
 #include <string.h>
 
 int fourier_transform(double complex* X, long N, int aux) {
+  assert((N & (N - 1)) == 0);  // Must be a power of two
   bit_reversal_permutation(X, N);
-  fft(X, N);
+  double complex* omegas = gen_omegas(N, ((aux < 0) ? true : false));
+  if (omegas == NULL) return 1;
+  fft(omegas, X, N);
+  if (aux < 0)
+    for (int j = 0; j < N; j++) X[j] /= N;
+  free(omegas);
   return 0;
 }
 
-unsigned int bit_length(unsigned int x) {
+unsigned int bit_length(unsigned long x) {
   unsigned int bits = 0;
   while (x) {
     bits++;
@@ -22,21 +28,21 @@ unsigned int bit_length(unsigned int x) {
   return bits;
 }
 
-unsigned int bit_reverse(unsigned int x, unsigned int n) {
-  unsigned int r = 0;
+unsigned long bit_reverse(unsigned long x, unsigned int n) {
+  unsigned long r = 0;
   for (unsigned int i = 0; i < n; ++i) {
     if (x & (1 << i)) r |= 1 << ((n - 1) - i);
   }
   return r;
 }
 
-void bit_reversal_permutation(double complex* x, int N) {
+void bit_reversal_permutation(double complex* x, long N) {
   // Don't forget bit_length is one indexed!
   int bl = bit_length(N) - 1;
 
   // We can skip the first and last index, they never need to be moved
-  for (int i = 1; i < N - 1; i++) {
-    int ri = bit_reverse(i, bl);
+  for (long i = 1; i < N - 1; i++) {
+    long ri = bit_reverse(i, bl);
     if (i < ri) {
       double complex temp = x[i];
       x[i] = x[ri];
@@ -45,19 +51,30 @@ void bit_reversal_permutation(double complex* x, int N) {
   }
 }
 
-void fft(double complex* X, int N) {
+double complex* gen_omegas(long N, bool inverse) {
+  int Nth = (bit_length(N) - 1);  // Log2(N)
+  double complex* omegas = malloc(sizeof(double complex) * Nth);
+  if (omegas == NULL) return NULL;
+  omegas[0] = CMPLX(-1, 0);
+  omegas[1] = CMPLX(0, -1);
+  for (int j = 2; j < Nth; j++) omegas[j] = csqrt(omegas[j - 1]);
+  if (inverse)
+    for (int j = 1; j < Nth; j++) omegas[j] = conj(omegas[j]);
+  return omegas;
+}
+
+void fft(double complex omegas[], double complex X[], long N) {
   assert((N & (N - 1)) == 0);  // Must be a power of two
-  double complex omega = CMPLX(-1, 0);
-  for (int n = 2; n <= N; n *= 2) {
-    double complex root_of_unity = omega;
-    for (int j = 0; j < n / 2; j++) {
-      for (int k = 0; k < N; k += n) {
+  for (long n = 2; n <= N; n *= 2) {
+    double complex omega = omegas[bit_length(n) - 2];
+    double complex root_of_unity = 1;
+    for (long j = 0; j < n / 2; j++) {
+      for (long k = 0; k < N; k += n) {
         double complex product = root_of_unity * X[k + j + n / 2];
         X[k + j + n / 2] = X[k + j] - product;
         X[k + j] = X[k + j] + product;
       }
       root_of_unity *= omega;
     }
-    omega = csqrt(omega);
   }
 }
--- fft.h
+++ fft.h
@@ -5,6 +5,7 @@
 #define DFT_H_INCLUDED
 
 #include <complex.h>
+#include <stdbool.h>
 
 /**
  * @brief Performs the Fourier transform. This interface is intended to be used
@@ -13,9 +14,10 @@
  *
  * @param X Input dataset, will be overwritten by results.
  * @param N Size of input dataset, must be a power of two.
- * @param aux Auxillary information, unused in this implementation.
+ * @param aux Auxillary information, if less than 0 the inverse Fourier
+ * transform will be performed instead.
  *
- * @return  0 on success, this implementation will never reasonably fail.
+ * @return  0 on success, 1 on failure (usually a malloc failure).
  */
 int fourier_transform(double complex* X, long N, int aux);
 
@@ -26,15 +28,28 @@ int fourier_transform(double complex* X, long N, int aux);
  * @param x Input dataset.
  * @param N Size of input dataset.
  */
-void bit_reversal_permutation(double complex* x, int N);
+void bit_reversal_permutation(double complex* x, long N);
+
+/**
+ * @brief Generates the lookup table for first nontrivial roots of unity for
+ * each stage.
+ *
+ * @param N Size of dataset that will be operated on, must be a power of two.
+ * @param inverse If true the table will be generated for performing the inverse
+ * FFT.
+ * @return double* A dynamically allocated array of values, needs to be free'd.
+ */
+double complex* gen_omegas(long N, bool inverse);
 
 /**
  * @brief The fast Fourier transform. Input is expected to be in
  * bit-reversal-permutation order.
  *
+ * @param omegas Lookup table for first nontrivial roots of unity for each
+ * butterfly stage.
  * @param X Input dataset, will be overwritten by results.
  * @param N Size of input dataset, must be a power of two.
  */
-void fft(double complex* X, int N);
+void fft(double complex* omegas, double complex* X, long N);
 
 #endif
